4861437e013d3af3b7732a0eabd3c43a96043a4f
Thomas Testa <ttesta99@yahoo.com>
2026-01-15 14:28:24 -0500

    Starting List File

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
+import { ListView } from '@/components/refine-ui/views/list-view'
+import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Search } from 'lucide-react'
+import { useState } from 'react'
 
 const SubjectsList = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedDepartment, setSelectedDepartment] = useState('all');
   return (
-    <div>
-      List
-    </div>
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects</h1>
+
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className='search-icon'/>
+            <Input 
+              type='text'
+              placeholder='Search by name...'
+              className='pl-10 w-full'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 w-full sm:w-auto">
+            <Select value={selectedDepartment} onValueChange={setSelectedDepartment}>
+              <SelectTrigger>
+                <SelectValue placeholder="Filter by department"/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All Deparments</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </ListView>
   )
 }
 
