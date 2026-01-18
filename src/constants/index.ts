53b5e37accfb3a5bf85f785d314c35d145425213
Thomas Testa <ttesta99@yahoo.com>
2026-01-18 12:02:40 -0500

    implement subject table

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const DEPARTMENTS = [
+    'Computer Science', 
+    'Math',
+    'English'
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept, 
+    label: dept,
+}));
--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,13 +1,74 @@
+import { CreateButton } from '@/components/refine-ui/buttons/create'
+import { DataTable } from '@/components/refine-ui/data-table/data-table'
 import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
 import { ListView } from '@/components/refine-ui/views/list-view'
+import { Badge } from '@/components/ui/badge'
 import { Input } from '@/components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { DEPARTMENT_OPTIONS } from '@/constants'
+import { Subject } from '@/types'
+import { useTable } from '@refinedev/react-table'
+import { ColumnDef } from '@tanstack/react-table'
 import { Search } from 'lucide-react'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 const SubjectsList = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedDepartment, setSelectedDepartment] = useState('all');
+
+  const departmentFilters = selectedDepartment === 'all' ? [] : [
+    {field: 'department', operator: 'eq' as const, value: selectedDepartment}
+  ]
+
+  const searchFilters = searchQuery ? [
+    {field: 'name', operator: 'contains' as const, value: searchQuery}
+  ] : [];
+
+  const subjectTable = useTable<Subject>({
+    columns: useMemo<ColumnDef<Subject>[]>(() => [
+      {
+        id: 'code', 
+        accessorKey: 'code', 
+        size: 100, 
+        header: () => <p className="column-title ml-2">Code</p>,
+        cell: ({getValue}) => <Badge>{getValue<string>()}</Badge>
+      },
+      {
+        id: 'name', 
+        accessorKey: 'name', 
+        size: 200,
+        header: () => <p className="column-title">Name</p>,
+        cell: ({getValue}) => <span className="text-foreground">{getValue<string>()}</span>,
+        filterFn: 'includesString',
+      },
+      {
+        id: 'department', 
+        accessorKey: 'department', 
+        size: 150,
+        header: () => <p className="column-title">Department</p>, 
+        cell: ({getValue}) => <Badge variant="secondary">{getValue<string>()}</Badge>
+      },
+      {
+        id: 'description', 
+        accessorKey: 'description',
+        header: () => <p className="column-title">Description</p>,
+        cell: ({getValue}) => <span className="truncate line-clamp-2">{getValue<string>()}</span>
+      }
+    ], []),
+    refineCoreProps:{
+      resource: 'subjects', 
+      pagination: {pageSize: 10, mode: 'server'},
+      filters: {
+        permanent: [...departmentFilters, ...searchFilters],
+      },
+      sorters: {
+        initial: [
+          {field: 'id', order: 'desc'}
+        ]
+      },
+    }
+  });
+
   return (
     <ListView>
       <Breadcrumb />
@@ -34,11 +95,19 @@ const SubjectsList = () => {
               </SelectTrigger>
               <SelectContent>
                 <SelectItem value="all">All Deparments</SelectItem>
+                {DEPARTMENT_OPTIONS.map(department => (
+                  <SelectItem key={department.value} value={department.value}>{department.label}</SelectItem>
+                ))}
               </SelectContent>
             </Select>
+
+
+            <CreateButton />
           </div>
         </div>
       </div>
+
+      <DataTable table = {subjectTable}/>
     </ListView>
   )
 }
